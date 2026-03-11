1fd5ec32068c90b0c209d9ed23fcc7a5f578b0c4
Pyae Phyo Aung <ppatheroyalexpress@gmail.com>
2026-03-11 21:41:20 +0630

    Fix: Add Tailwind oxide optional dependencies

--- web/frontend/courierearn-web/next.config.ts
+++ web/frontend/courierearn-web/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    resolveAlias: {
+      '@tailwindcss/oxide': '@tailwindcss/oxide-linux-x64-gnu'
+    }
+  }
 };
 
 export default nextConfig;
